22a92bdc65cd2d0219f5042847c900e9edb87d5d
Anthony Urbina <aurbinava002@gmail.com>
2022-12-10 23:59:53 +0000

    saving prog

---
+++ code-signal/almostIncreasingSequence.js
@@ -0,0 +1,19 @@
+/*
+Given a sequence of integers as an array,
+determine whether it is possible to obtain
+a strictly increasing sequence by removing
+no more than one element from the array.
+*/
+
+/*
+create a counter variable
+loop through elements in the array
+a = i
+b = i + 1
+if b is not greater than a
+add 1 to the counter
+loop through all elements
+if counter is greater than 1
+return false
+else return true
+*/
