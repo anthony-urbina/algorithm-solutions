9a989b4238ee9c049c16cff895172592465b7b58
Anthony Urbina <aurbinava002@gmail.com>
2022-12-15 05:24:32 +0000

    found better solution

--- code-signal/isLucky.js
+++ code-signal/isLucky.js
@@ -31,9 +31,30 @@ function isLucky(num) {
   const str2 = arr2.map(str => parseInt(str));
   const num1 = str1.reduce((a, b) => a + b, 0);
   const num2 = str2.reduce((a, b) => a + b, 0);
+
   if (num1 === num2) {
     return true;
   } else {
     return false;
   }
+}
+
+// found a solution that's better than mine
+
+function solution(n) {
+  // converts n to a string
+  var s = n.toString();
+  var sum1 = 0;
+  var sum2 = 0;
+  // loop through string as long as i is less than HALF the length string
+  for (var i = 0; i < s.length / 2; i++) {
+    // convert element to integer
+    // incrementally assign the first half of elements to sum1
+    sum1 += parseInt(s[i]);
+    // convert element to integer
+    // incrementally assign the second half of elements to sum2
+    sum2 += parseInt(s[s.length - i - 1]);
+  }
+  // return true if equal, false if not equal
+  return sum1 === sum2;
 }
