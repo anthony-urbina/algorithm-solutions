b9b8271cda9b92d83c0690503d00810f8269573f
Anthony Urbina <aurbinava002@gmail.com>
2022-12-23 23:05:31 +0000

    addBorder: CodeSignal

---
+++ code-signal/addBorder.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-unused-vars */
+/*
+Given a rectangular matrix of characters, add a border of asterisks(*) to it.
+*/
+
+// loop through array
+// add '*' to start of element
+// add '*' to end of element
+// get length of first element
+// while i is less than length of first element
+// add a '*' to border
+// push border to picture
+// unshift border to picture
+// return picture
+
+function addBorder(picture) {
+  const mappedPicture = picture.map(element => `*${element}*`);
+  const length = mappedPicture[0].length;
+  let border = '';
+  let i = 0;
+  while (i < length) {
+    border += '*';
+    i++;
+  }
+  mappedPicture.push(border);
+  mappedPicture.unshift(border);
+  return mappedPicture;
+}
