4472d23bcc4c1e02506efa54de6cf82e48834c58
Anthony Urbina <aurbinava002@gmail.com>
2023-02-17 06:56:47 +0000

    saving progress

---
+++ code-signal/stringsRearrangement.js
@@ -0,0 +1,32 @@
+// eslint-disable-next-line no-unused-vars
+function rearrange(inputArray) {
+  // array of equal length strings
+  // possible to rearrande order of strings so that each
+  // consecutive pair differs by exactly 1 character
+  // return true if possible, false if not
+
+  // figure out all possible combinations
+  // align them like so
+  // run through each combination
+  // if one differs by 1, return true, else return false
+  // 0 1 2 3
+  // 4 digits 24 possible
+  // 0 1 2 3
+  // 0 1 3 2
+  // 0 2 3 1
+  // 0 2 1 3
+  // 0 3 1 2
+  // 0 3 2 1
+
+  // 0 1 2
+  // 3 digits 6 possible
+  // 0 1 2
+  // 0 2 1
+  // 1 2 0
+  // 1 0 2
+  // 2 0 1
+  // 2 1 0
+  // 2 digits 2 possible
+  // loop through all elements
+  //
+}
